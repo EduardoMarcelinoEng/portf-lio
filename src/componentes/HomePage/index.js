60a597f8e28c213b09224b38ecf9a4f9ff5692d3
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2021-02-13 20:41:11 -0300

    portfolio finalizado

--- src/componentes/HomePage/index.js
+++ src/componentes/HomePage/index.js
@@ -5,18 +5,18 @@ import './style.css';
 export default function HomePage() {
     return (
         <div className="home-page">
-            <a href="https://baralhosignos.herokuapp.com/"><Card 
+            <a href="https://baralhosignos.herokuapp.com/" target="_blank"><Card 
                 name="Baralho dos Signos"
                 description="Layout com informações dos signos"
                 tech="HTML, CSS, JavaScript, React"
                 url="projects/horoscopo/index.html"
             /></a>
-            <a href="https://numero-aleatorio.herokuapp.com/"><Card 
+            <a href="https://numero-aleatorio.herokuapp.com/" target="_blank"><Card 
                 name="Número Aleatório"
                 description="Ao clicar no botão, é mostrado na tela um número entre 1 e 100 de forma aleatória"
                 tech="HTML, CSS, JavaScript, React"
             /></a>
-            <a href=""><Card
+            <a href="https://consultorio-dentista.herokuapp.com/" target="_blank"><Card
                 name="Consultório de Dentista"
                 description="Layout de um site de consultório de dentista"
                 tech="HTML, CSS, JavaScript, React"
