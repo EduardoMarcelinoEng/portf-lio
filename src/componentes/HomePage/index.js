42f73c363875a57250435eb5e427e7520333f0fb
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2021-02-13 20:13:11 -0300

    link para os projetos. Falta consultorio de dentista

--- src/componentes/HomePage/index.js
+++ src/componentes/HomePage/index.js
@@ -5,22 +5,22 @@ import './style.css';
 export default function HomePage() {
     return (
         <div className="home-page">
-            <Card 
+            <a href="https://baralhosignos.herokuapp.com/"><Card 
                 name="Baralho dos Signos"
                 description="Layout com informações dos signos"
                 tech="HTML, CSS, JavaScript, React"
                 url="projects/horoscopo/index.html"
-            />
-            <Card 
+            /></a>
+            <a href="https://numero-aleatorio.herokuapp.com/"><Card 
                 name="Número Aleatório"
                 description="Ao clicar no botão, é mostrado na tela um número entre 1 e 100 de forma aleatória"
                 tech="HTML, CSS, JavaScript, React"
-            />
-            <Card
+            /></a>
+            <a href=""><Card
                 name="Consultório de Dentista"
                 description="Layout de um site de consultório de dentista"
                 tech="HTML, CSS, JavaScript, React"
-            />
+            /></a>
         </div>
     );
 }
